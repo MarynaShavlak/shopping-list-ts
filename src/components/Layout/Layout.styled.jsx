232ab89fa188c4793678f30aad5095517936040b
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-11-23 04:54:53 +0200

    add start readme

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import backImg from './main-back.png';
 
 export const LayoutStyled = styled.main`
   display: flex;
@@ -6,4 +7,10 @@ export const LayoutStyled = styled.main`
   padding-left: 124px;
   padding-top: 58px;
   padding-bottom: 57px;
+  max-width: 1366px;
+  margin: 0 auto;
+  background-image: url(${backImg});
+  background-size: contain;
+  background-position: right;
+  background-repeat: no-repeat;
 `;
